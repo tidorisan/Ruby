dec41f98df427daf4065b338adfb8e051990a701
natsumerika <natsumesansan@gmail.com>
2020-06-20 15:30:05 +0900

    [finish]目次

---
+++ 2-6/2-6.rb
@@ -0,0 +1,17 @@
+puts 100 + 5
+
+name  = "鈴木さん"
+
+puts "こんにちは#{name}"
+
+names = ["perl","ruby","Rython","swift"]
+puts names[1]
+
+# tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+# puts tall["太郎"]
+
+tall = {"太郎"=>185,"二郎"=>170, "花子"=>150}
+puts tall ["太郎"]
+
+
+
