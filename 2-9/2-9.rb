1d3b0bb964860bc475dec492b349707b6284b1e1
natsumerika <natsumesansan@gmail.com>
2020-06-21 11:57:47 +0900

    [finish]目次

---
+++ 2-9/2-9.rb
@@ -0,0 +1,55 @@
+# def method_name
+	
+# # end
+
+
+# def hello_world
+# 	"hello_world"
+	
+# end
+# # def method_name
+	
+# end
+
+
+# # def hello_world
+# # 	"hello,world"
+	
+# # end
+
+# # puts hello_world
+
+# name = John
+
+# def greeting(name)
+#   "Hello, #{name}!" # nameの中には、文字列'John'が入っている
+# end 
+ 
+# puts greeting('John')
+
+
+# # def greeting(name)
+# 	"hello,#{name}"
+	
+# end
+
+# # puts greeting(John)
+
+# puts hello_world
+
+# name = "John"
+
+# # ""忘れない
+
+# def greeting(name)
+#   "Hello, #{name}!" # nameの中には、文字列'John'が入っている
+# end 
+ 
+# # puts greeting('John') # ここでは'John'を引数として渡している
+
+# greeting('John') 
+
+puts 10 +"1".to_i
+
+puts 10.to_s +  "1"
+
