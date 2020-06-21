04b999e7d8f131f172d0546f49a05807ac9e9407
natsumerika <natsumesansan@gmail.com>
2020-06-21 10:31:45 +0900

    [finish]2-8

---
+++ 2-8/ruby.rb
@@ -0,0 +1,96 @@
+puts "計算を始めます#"
+
+puts "何回計算を繰り返しますか？"
+
+input = gets.to_i
+
+i = 1 
+
+while i <= input do 
+
+puts "#{i}回目の計算" 
+# ここの考え方
+
+	puts "2つの値を入力してください" 
+
+	a = gets.to_i
+
+	b = gets.to_i
+
+# putsは/
+
+	puts "a=#{a}"
+	puts "b=#{b}"
+
+	puts "計算結果を出力します"
+
+	puts "a+b=#{a + b}"
+
+	puts "a-b=#{a - b}"
+	puts "a*b=#{a * b}"
+	puts "a/b=#{a / b}"
+
+	# 最後にこれかく　忘れない
+
+	i+1
+
+end
+
+puts "計算を終了します"
+
+# step1 = gets.to_1
+
+# puts step1
+# ここ大事
+# input = gets.to_i
+
+# i = 1 
+
+# while i <= input do
+# 	# 直接１じゃダメなのか？　変数する意味
+
+# 	puts "1回目の計算"
+# 	puts "2つの値を入力してください"
+
+# 	a = gets.to_i
+# 	b = gets.to_i
+# 	# なぜ　puts は
+# 	puts "a=#{a}"
+# 	puts "b=#{b}" 
+
+# 	puts "計算結果を出力します" 
+
+
+
+
+
+# # puts a
+
+# # puts  b
+
+# puts "a=#{a}"
+
+# # 文字型　出力　代入した後に行う場合　文字型の展開　＃括弧を用いる
+
+# puts "b+#{b}"
+
+
+# puts "計算結果を出力します"
+
+# puts "a+b=#{a+b}"
+
+# puts "a-b=#{step2-step3}"
+
+# puts " a*b=#{a*b}"
+
+# puts "a/b=#{a/b}"
+
+
+
+# puts "2回目の計算"
+# puts "2つの値を入力してください"
+
+
+
+
+
